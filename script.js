b7f3ad93a333c0487c9bb8141ce01df2f8b25baa
Philippe Recto <precto1285@gmal.com>
2019-10-11 20:02:55 -0700

    added error message for empty input field

--- script.js
+++ script.js
@@ -9,16 +9,25 @@ var setListOne = document.getElementById("targetListOne");
 var setListTwo = document.getElementById("targetListTwo");
 var setListThree = document.getElementById("targetListThree");
 
+// Event Listener to add Songs to Set List/s
 button.addEventListener('click', addSong, true);
 
+// Add Song Function
 function addSong(e) {
   console.log(songInput.value);
   console.log(artistInput.value);
   console.log(setListNum.value);
+  if (songInput.value === '' || artistInput.value === '') {
+    // Error Message if input field is empty
+    var errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerHTML = "Please Add Song and Artist";
+    setTimeout(function () { errorMsg.innerHTML = ""; }, 3000);
+  }
   clearForm();
   e.preventDefault();
 };
 
+// Clear Input Fields
 function clearForm() {
   songInput.value = ' ';
   artistInput.value = ' ';
