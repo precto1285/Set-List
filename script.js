17928a959e924c65b8380f999fd19851b5fa2fe4
Philippe Recto <precto1285@gmal.com>
2019-10-11 19:53:30 -0700

    created variables, event listener and initial functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Global Variables
+var songInput = document.getElementById("songName");
+var artistInput = document.getElementById("songArtist");
+var setListNum = document.getElementById("setListNum");
+var button = document.querySelector(".btn");
+
+// Table Set List Targets
+var setListOne = document.getElementById("targetListOne");
+var setListTwo = document.getElementById("targetListTwo");
+var setListThree = document.getElementById("targetListThree");
+
+button.addEventListener('click', addSong, true);
+
+function addSong(e) {
+  console.log(songInput.value);
+  console.log(artistInput.value);
+  console.log(setListNum.value);
+  clearForm();
+  e.preventDefault();
+};
+
+function clearForm() {
+  songInput.value = ' ';
+  artistInput.value = ' ';
+  setListNum.value = 1;
+}
